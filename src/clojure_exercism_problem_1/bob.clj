3efe47071b8ef59147ed49eb868c1e14de47c7d1
tom.vaughan <tom@opower.com>
2014-10-15 19:28:05 -0400

    Enough for tonight; got 9/14 tests passing

--- src/clojure_exercism_problem_1/bob.clj
+++ src/clojure_exercism_problem_1/bob.clj
@@ -1,6 +1,29 @@
 (ns clojure_exercism_problem_1.bob
   (:gen-class))
 
+;(require '[clojure.string :as str])
+(import 'org.apache.commons.lang3.StringUtils)
+
+(def RESPONSE_QUESTION "Sure.")
+(def RESPONSE_YELL     "Whoa, chill out!")
+(def RESPONSE_SILENCE  "Fine. Be that way!")
+(def RESPONSE_OTHER    "Whatever.")
+
+(defn isSilence [input]
+  (StringUtils/isBlank input))
+
+(defn isQuestion [input]
+  (.endsWith input "?"))
+
+(defn isYell [input]
+  (StringUtils/isAllUpperCase (.replaceAll input "\\d" "")))
+
 (defn response-for [input]
-  input)
+  (cond
+    (isQuestion input) RESPONSE_QUESTION
+    (isYell     input) RESPONSE_YELL
+    (isSilence  input) RESPONSE_SILENCE
+    :else RESPONSE_OTHER))
+
+
 
