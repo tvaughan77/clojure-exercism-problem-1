69ca618f3c41cdc5df36ca208b26a8c593643da8
tom.vaughan <tom@opower.com>
2014-10-15 18:46:48 -0400

    Figured out namespaces and getting the test to fail with a minimal bob.clj file

---
+++ src/clojure_exercism_problem_1/bob.clj
@@ -0,0 +1,11 @@
+(ns clojure_exercism_problem_1.bob
+  (:gen-class))
+
+(defn response-for [input]
+  input)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
