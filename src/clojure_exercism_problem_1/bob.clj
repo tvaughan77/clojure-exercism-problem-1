0c09342a8e813b221be52fd439cf3681d419a499
tom.vaughan <tom@opower.com>
2014-10-16 18:05:55 -0400

    Comments and removing an unused import

--- src/clojure_exercism_problem_1/bob.clj
+++ src/clojure_exercism_problem_1/bob.clj
@@ -1,7 +1,6 @@
 (ns clojure_exercism_problem_1.bob
   (:gen-class))
 
-;(require '[clojure.string :as str])
 (import 'org.apache.commons.lang3.StringUtils)
 
 (def RESPONSE_QUESTION "Sure.")
@@ -9,9 +8,11 @@
 (def RESPONSE_SILENCE  "Fine. Be that way!")
 (def RESPONSE_OTHER    "Whatever.")
 
+; Any empty string counts as silence (prolonged or otherwise)
 (defn isSilence [input]
   (StringUtils/isBlank input))
 
+; Anything that ends with a question mark is a question
 (defn isQuestion [input]
   (.endsWith input "?"))
 
