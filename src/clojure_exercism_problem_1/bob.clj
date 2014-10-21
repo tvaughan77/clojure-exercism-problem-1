8f7500ffa8d901c785e71e8050da2d4649b033d0
tom.vaughan <tom@opower.com>
2014-10-21 10:52:43 -0400

    Removing java StringUtils in favor of a pure-Clojure solution

--- src/clojure_exercism_problem_1/bob.clj
+++ src/clojure_exercism_problem_1/bob.clj
@@ -1,7 +1,7 @@
 (ns clojure_exercism_problem_1.bob
   (:gen-class))
 
-(import 'org.apache.commons.lang3.StringUtils)
+(require '[clojure.string :as str])
 
 (def RESPONSE_QUESTION "Sure.")
 (def RESPONSE_YELL     "Whoa, chill out!")
@@ -10,15 +10,23 @@
 
 ; Any empty string counts as silence (prolonged or otherwise)
 (defn isSilence [input]
-  (StringUtils/isBlank input))
+  (str/blank? input))
 
 ; Anything that ends with a question mark is a question
 (defn isQuestion [input]
   (.endsWith input "?"))
 
+; Returns the input string with nothing but alphabetic characters in it
+(defn makeJustAlpha [s]
+  (.replaceAll s "[^a-zA-Z]" ""))
+
 ; Yelling is where all alphanumeric characters are upper case
 (defn isYell [input]
-  (StringUtils/isAllUpperCase (.replaceAll input "[^a-zA-Z]" "")))
+  (let [just-alpha (makeJustAlpha input)
+        upper-comp (str/upper-case just-alpha)]
+     (and 
+        (= just-alpha upper-comp)
+        (not (str/blank? just-alpha)))))
 
 (defn response-for [input]
   (cond
